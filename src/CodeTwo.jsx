d874fdeeba3a275054f93b0467e78a636a495bef
Office Demo <sahupragya406@gmail.com>
2026-02-05 18:06:11 +0530

    Re-correction

--- src/CodeTwo.jsx
+++ src/CodeTwo.jsx
@@ -36,9 +36,7 @@ function CodeTwo() {
       {/* <button onClick={()=>setValue(10000)} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">{value}</button> */}
       <div className="flex items-center justify-center mt-12 gap-10 ">
 
-        {
-          user.users.map((item, i)=>{
-            return(
+        
               <div key={i}>
                 <table>
                   <thead>
@@ -49,19 +47,19 @@ function CodeTwo() {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
+                    {user.users.map((item, i) => (
+                    <tr key={i}>
                        <td> {item.id}</td> 
                       <td> {item.name}</td> 
                     <td> {item.city}</td> 
                     </tr> 
+                    ))}
                   </tbody>  
                 </table>
               
   
               </div>
-            )
-          })
-        }
+            
 
         {/* {
           praful.map((item, i) => {
