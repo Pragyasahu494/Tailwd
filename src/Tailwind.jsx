4b0784fc57b49b62e29319174dc1c146a7d7124d
Office Demo <sahupragya406@gmail.com>
2026-02-14 19:54:45 +0530

    Added new lines in TW

--- src/Tailwind.jsx
+++ src/Tailwind.jsx
@@ -2,8 +2,12 @@ export default function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
-        Hello Tailwind 🚀
+        Hello Tailwind!!!🚀
       </h1>
+      <h1 className="text-3xl font-bold text-red-500">
+  Tailwind is working!
+</h1>
+
     </div>
   );
 }
