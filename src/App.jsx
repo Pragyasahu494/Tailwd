c1c8956daf76155e34d1bb0f5bf51d9b04cb767b
Office Demo <sahupragya406@gmail.com>
2026-02-14 20:22:25 +0530

    error solving

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import CodeOne from './CodeOne'
-import Navbar from './Navbar'
-import Docs from './Docs'
-import CodeTwo from './CodeTwo'
-import CodeThree from './CodeThree'
-import NewsApp from './NewsApp'
-import WeatherApp from './WeatherApp'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Tailwind from "./Tailwind";
+import NewsApp from "./NewsApp";
+import WeatherApp from "./WeatherApp";
+import Docs from "./Docs";
+import CodeOne from "./CodeOne";
+import CodeTwo from "./CodeTwo";
+import CodeThree from "./CodeThree";
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<CodeTwo />} />
-          <Route path='/news' element={<NewsApp />} />
-          <Route path='/weather' element={<WeatherApp />} />
-          <Route path='/docs' element={<Docs />} />
-          <Route path='/tailwd' element={<Tailwd />} />
-          <Route path='/service' element={<CodeOne />}>
-            <Route path='web' element={<CodeThree />} />
-          </Route>
-
-          <Route path='/contact' element={<CodeOne />} />
-        </Routes>
-      </BrowserRouter>
-
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/tailwind" element={<Tailwind />} />
+        <Route path="/news" element={<NewsApp />} />
+        <Route path="/weather" element={<WeatherApp />} />
+        <Route path="/docs" element={<Docs />} />
+        <Route path="/service" element={<CodeOne />} />
+        <Route path="/web" element={<CodeThree />} />
+        <Route path="/contact" element={<CodeTwo />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/Tailwind.jsx
+++ src/Tailwind.jsx
@@ -1,4 +1,4 @@
-export default function App() {
+export default function Tailwind() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
